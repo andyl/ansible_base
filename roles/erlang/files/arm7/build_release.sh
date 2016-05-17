9925cd82243a9339aad7b064b1d438318ded5a33
andyl <andy@r210.com>
2016-05-16 19:24:42 -0700

    1605161924

---
+++ roles/erlang/files/arm7/build_release.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# build erlang on arm7 (32-bit RPi/Raspbian and Odroid XU4)
+
+# from http://elinux.org/Erlang
+
+# TO USE:
+# 1) copy the script to a build machine, and run it
+# 2) save the release image to the current directory 
+#    x-ansible/roles/erlang/files/arm7/<release_image>
+
+version=18.3
+otpfile=otp_src_$version
+release=erl_arm7_$version
+rel_dir=`pwd`/$release
+rel_tar=$release.tar.gz
+
+sudo apt-get update
+sudo apt-get install wget
+sudo apt-get install libssl-dev
+sudo apt-get install ncurses-dev
+sudo apt-get install m4
+wget http://www.erlang.org/download/$otpfile.tar.gz
+mkdir -p $rel_dir
+tar -xzvf $otpfile.tar.gz
+cd $otpfile
+./configure
+make
+make install DESTDIR=$rel_dir
+cd ..
+tar cfz $rel_tar $release
+echo "MAKE && MAKE RELEASE - DONE"
+echo "RELEASE IMAGE IS $rel_tar"
