57f9e6e329b8b343589fb6dc0b94708482d083b9
andyl <andy@r210.com>
2018-12-11 20:10:00 -0800

    1812112009

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,13 +2,14 @@
 
 # Vagrant/Ansible Bootstrap Script
 
-# This bootstrap script does two things:
+# This bootstrap script:
 # 1) gets the `ansible` roles onto the target machine
-# 2) installs ansible onto the target machine
+# 2) writes the ANSIBLE directory to the `/vagrant` fireshare directory
+# 3) installs ansible onto the target machine
 
 # The script is meant to be run by the Vagrant shell-provisioner.
 #
-# source <(curl -s <SCRIPT URL>)
+# source <(curl -sL <SCRIPT URL>)
 # 
 
 echo "=== INSTALL ANSIBLE ROLES"
@@ -25,7 +26,7 @@ else
   git clone https://github.com/andyl/x-ansible.git > /dev/null 2>&1
 fi
 
-echo "=== SYNC /vagrant/ANSIBLE"
+echo "=== WRITE /vagrant/ANSIBLE"
 mkdir -p /vagrant
 rm -rf /vagrant/ANSIBLE
 cp -r x-ansible/ANSIBLE /vagrant
@@ -34,5 +35,4 @@ echo "=== INSTALL ANSIBLE EXECUTABLE"
 cd x-ansible/bin
 ./install_ansible
 echo "DONE."
-
 
