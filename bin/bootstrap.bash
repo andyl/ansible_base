5e192af598dccefb4f88305e685a5530a12382c1
andyl <andy@r210.com>
2018-12-15 11:32:33 -0800

    Update

--- bin/bootstrap.bash
+++ bin/bootstrap.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Vagrant/Ansible Bootstrap Script
 
@@ -13,7 +13,7 @@
 # 
 
 function writekey() {
-  [ $# == 1 ] || return 0
+  [[ $# == 1 ]] || return 0
   echo $1 > /tmp/hostkey.pub
 }
 
