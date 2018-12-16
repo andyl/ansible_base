b338381ea49ed88472f182cb981d33769e806fe8
andyl <andy@r210.com>
2018-12-15 21:43:22 -0800

    1812152143

--- roles/ARCHIVE/RPI/cli_locale/templates/locale_default.sh
+++
@@ -1,4 +0,0 @@
-# Locale Default
-# {{ansible_managed}}
-# {{template_path}}
-LANGUAGE="en_US.UTF-8"
--- roles/ARCHIVE/RPI/direwolf/files/direwolf-execute.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run direwolf after a delay
-sleep 120;
-/usr/local/bin/direwolf -c /etc/direwolf.conf;
