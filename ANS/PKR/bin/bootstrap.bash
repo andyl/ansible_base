284720b8384ac9ec56bc6d4d6c355d9664babacb
andyl <andy@r210.com>
2024-06-25 16:21:39 -0700

    Update packer bootstrap

--- ANS/PKR/bin/bootstrap.bash
+++ ANS/PKR/bin/bootstrap.bash
@@ -12,34 +12,7 @@
 # source <(curl -sL <SCRIPT URL>)
 # 
 
-function writekey() {
-  [[ $# == 1 ]] || return 0
-  echo "=== WRITING KEY"
-  echo $1 > /tmp/hostkey.pub
-}
-
-# echo "=== INSTALL ANSIBLE ROLES"
-# mkdir -p util
-# cd util
-# if [[ -d x-ansible ]] ; then
-#   echo "Updating roles"
-#   cd x-ansible
-#   sudo git pull > /dev/null 2>&1
-#   cd ..
-# else
-#   echo "Cloning roles"
-#   sudo rm -rf x-ansible
-#   git clone --depth=1 https://github.com/andyl/x-ansible.git > /dev/null 2>&1
-# fi
-
-# to prevent copying the ANSIBLE directory, add
-# `export NOANSIBLE=true` to your shell provisioner
-# echo "=== COPY /vagrant/ANSIBLE"
-# mkdir -p /vagrant
-# rm -rf /vagrant/ANSIBLE
-# [ -z $NOANSIBLE ] && cp -r x-ansible/ANSIBLE /vagrant
-
 echo "=== INSTALL ANSIBLE EXECUTABLE"
-cd /vvm/x-ansible/ANS_VVM/bin
+cd /x-ansible/ANS_PKR/bin
 ./install_ansible
 
